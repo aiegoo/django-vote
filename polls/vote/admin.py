32184e4d4f19461c374dbd368cd25ecddd58fead
aiegoo <eozz21@gmail.com>
2019-10-15 12:21:35 +0000

    trying to fix tabularInline not being defined

--- polls/vote/admin.py
+++ polls/vote/admin.py
@@ -1,21 +1,27 @@
 from django.contrib import admin
 from .models import Question, Choice
+from django.contrib.auth.models import User
+
+# from django.contrib.admin import TabularInline
+from django.urls import reverse
 
 
 # Register your models here.
 class ChoiceInline(admin, TabularInline):
-     model = Choice
-     extra = 3
+    model = Choice
+    extra = 3
+
 
 class QuestionAdmin(admin.ModelAdmin):
-     fieldsets = [
-          (None, {'fields': ['question_text']}),
-          ('Date information', {'fields': ['pub_date']}),
-     ]
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+
 list_display = ('question_text', 'pub_date', 'was_published_recently')
 inlines = [ChoiceInline]
 list_filter = ['pub_date']
 search_fields = ['question_text']
-
 
 admin.site.register(Question, QuestionAdmin)
